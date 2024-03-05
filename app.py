2fda7032e11912af6721907975384cd30e10f630
Ronald-Kaue <133374965+Ronald-Kaue@users.noreply.github.com>
2024-03-05 13:37:33 +0000

    Melhorando meu repositório

--- app.py
+++ app.py
@@ -8,11 +8,23 @@ def index():
 
 @app.route('/compras')
 def compras():
-    return render_template('compras.html')
+    return render_template('compras.html', item1 = "Farinha", item2 = "Cuscuz")
 
 @app.route('/mercado')
 def mercado():
     return render_template('mercado.html')
+
+@app.route('/gastos', defaults = {"mes":"Janeiro", "valor":"None"})
+@app.route('/gastos/<mes>/<valor>')
+def gastos(mes, valor):
+    return render_template('gastos.html', a=mes, b=valor)
+
+@app.route('/dobro', defaults = {"n": 1})
+@app.route('/dobro/<float:n>')
+@app.route('/dobro/<int:n>')
+def dobro(n):
+    resul = n*2
+    return render_template('dobro.html', n = n, result = resul)
 
 if __name__ == '__main__':
     app.run()
